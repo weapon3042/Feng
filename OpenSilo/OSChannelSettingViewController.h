05e3471018ff8c2072d342ee3c8d263b2ff9e132
linkedIn API <Kelvin@Kelvins-Mac-mini.local>
2014-08-04 15:21:25 -0700

    added channel, room view controllers

---
+++ OpenSilo/OSChannelSettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  OSChannelSettingViewController.h
+//  OpenSilo
+//
+//  Created by Peng Wan & Elmir Kouliev on 8/4/14.
+//  Copyright (c) 2014 OpenSilo Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSChannelSettingViewController : UIViewController
+
+@end
