85a26b6df65bdc145f618de3b2794f0ac1a4bac0
linkedIn API <Kelvin@Kelvins-Mac-mini.local>
2014-08-03 20:46:26 -0700

    ui adjustments

---
+++ OpenSilo/OSChannelViewController.h
@@ -0,0 +1,13 @@
+//
+//  OSChannelViewController.h
+//  OpenSilo
+//
+//  Created by Kelvin Lam on 8/2/14.
+//  Copyright (c) 2014 IDesign Network Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSChannelViewController : UIViewController
+
+@end
---
+++ OpenSilo/OSRoomViewController.h
@@ -0,0 +1,13 @@
+//
+//  OSRoomViewController.h
+//  OpenSilo
+//
+//  Created by Kelvin Lam on 8/2/14.
+//  Copyright (c) 2014 IDesign Network Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OSRoomViewController : UIViewController
+
+@end
