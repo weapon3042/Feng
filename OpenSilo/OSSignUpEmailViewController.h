af599851aa88e742a050a6fff0ed2dd48a287927
linkedIn API <Kelvin@Kelvins-Mac-mini.local>
2014-08-06 16:24:44 -0700

    1. dynamic segment control. 2. signup email ui view. 3. sign updetails ui view.

--- OpenSilo/OSSignUpEmailViewController.h
+++ OpenSilo/OSSignUpEmailViewController.h
@@ -2,8 +2,8 @@
 //  OSSignUpEmailViewController.h
 //  OpenSilo
 //
-//  Created by Kelvin Lam on 8/6/14.
-//  Copyright (c) 2014 IDesign Network Inc. All rights reserved.
+//  Created by Peng Wan & Elmir Kouliev on 8/6/14.
+//  Copyright (c) 2014 OpenSilo Co. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
